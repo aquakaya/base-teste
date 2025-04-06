b92453864e3d85252e4439def504037d442b3d78
Tonic Munodawafa <161943111+tonicmeef@users.noreply.github.com>
2025-04-06 11:40:03 +0200

    Update updater.js

--- plugins/updater.js
+++ plugins/updater.js
@@ -41,7 +41,7 @@ const update = async (m, Matrix) => {
 
             // Fetch latest commit hash
             const { data: commitData } = await axios.get(
-                "https://api.github.com/repos/XdTechPro/JAWAD-MD/commits/main"
+                "https://api.github.com/repos/tonicmeef/TONIC-MD/commits/main"
             );
             const latestCommitHash = commitData.sha;
 
@@ -55,7 +55,7 @@ const update = async (m, Matrix) => {
                 return editMessage("```✅ Bot is already up to date!```");
             }
 
-            await editMessage("```🚀 New update found! Downloading...```");
+            await editMessage("``` New update found! Downloading...```");
 
             // Download latest ZIP
             const zipPath = path.join(process.cwd(), "latest.zip");
@@ -63,7 +63,7 @@ const update = async (m, Matrix) => {
             
             const response = await axios({
                 method: 'get',
-                url: "https://github.com/XdTechPro/JAWAD-MD/archive/main.zip",
+                url: "https://github.com/tonicmeef/TONIC-MD/archive/main.zip",
                 responseType: 'stream'
             });
 
@@ -84,7 +84,7 @@ const update = async (m, Matrix) => {
             await editMessage("```🔄 Replacing files...```");
 
             // Replace files while skipping important configs
-            const sourcePath = path.join(extractPath, "JAWAD-MD-main");
+            const sourcePath = path.join(extractPath, "TONIC-MD-main");
             await copyFolderSync(sourcePath, process.cwd(), ['package.json', 'config.cjs', '.env']);
 
             // Update package.json with new commit hash
