a10ff0b3d9017bd137bb27548fc13bd2867f3094
Tonic Munodawafa <161943111+tonicmeef@users.noreply.github.com>
2025-04-06 11:38:41 +0200

    Add files via upload

---
+++ plugins/ping.js
@@ -0,0 +1,44 @@
+import config from '../config.cjs';
+
+const ping = async (m, Matrix) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+
+  if (cmd === "ping") {
+    const start = new Date().getTime();
+
+    const reactionEmojis = ['🔥', '⚡', '🚀', '💨', '🎯', '🎉', '🌟', '💥', '🕐', '🔹'];
+    const textEmojis = ['⚡️'];
+
+    const reactionEmoji = reactionEmojis[Math.floor(Math.random() * reactionEmojis.length)];
+    let textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+
+    // Ensure reaction and text emojis are different
+    while (textEmoji === reactionEmoji) {
+      textEmoji = textEmojis[Math.floor(Math.random() * textEmojis.length)];
+    }
+
+    await m.React(textEmoji);
+
+    const end = new Date().getTime();
+    const responseTime = (end - start) / 1000;
+
+    const text = `*TONIC-MD SPEED=>: ${responseTime.toFixed(2)}ms*`;
+
+    await Matrix.sendMessage(m.from, {
+      text,
+      contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363374632065395@newsletter',
+          newsletterName: "Tᴏɴɪᴄ Tᴇᴄʜ Iɴᴄ.",
+          serverMessageId: 143
+        }
+      }
+    }, { quoted: m });
+  }
+};
+
+export default ping;
